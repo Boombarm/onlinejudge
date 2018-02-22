b53828176e1ff9405cfbaa1147c3bd10f852a411
Boombarm <boombarm@gmail.com>
2018-02-23 01:31:52 +0700

    23-02-2018

---
+++ src/uri/P2708_Tourists_in_the_Huacachina_Park.py
@@ -0,0 +1,35 @@
+# =======================================================================================================================
+# author : Teerapat Phokhonwong
+# Onlinejudge: URI Online Judge
+# Categories:
+# Problem:
+# Link:
+# Timelimit: 1 sec
+# Status:
+# Submission:
+# Runtime:
+# Solution:
+# Note:
+# =======================================================================================================================
+import sys
+
+adventure_tourists = 0
+adventure_jeep = 0
+while True:
+    # Reade Line
+    input_ = sys.stdin.readline()
+    if input_ == "ABEND\n":
+        break
+
+    t = int(sys.stdin.readline())
+    if input_ == "SALIDA\n":
+        adventure_jeep += 1
+        adventure_tourists += t
+    else:
+        if adventure_jeep > 0:
+            adventure_jeep -= 1
+        if adventure_tourists >= t:
+            adventure_tourists -= t
+
+print(adventure_tourists)
+print(adventure_jeep)
