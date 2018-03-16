44855271715955a523198a001b74247233b994c3
Boombarm <boombarm@gmail.com>
2018-03-16 20:46:29 +0700

    16032018

---
+++ src/codechef/TLG_The_Lead_Game.py
@@ -0,0 +1,28 @@
+# author : Teerapat Phokhonwong
+# Online judge: URI Online Judge
+# Categories: Practice(Beginner)
+# Problem: TLG - The Lead Game
+# Link: https://www.codechef.com/problems/TLG
+# Timelimit: 1 sec
+# Status: AC
+# Submission: 2018-03-16 19:14:07
+# MEM: 27.1M
+# Runtime: 0.03
+# Solution:
+# Note:
+
+
+n = int(input())
+p_win = maxLead = p1 = p2 = 0
+for i in range(1, n):
+    a, b = map(int, input().split())
+    p1 = p1 + a
+    p2 = p2 + b
+    if p1 > p2 and p1 - p2 > maxLead:
+        p_win = 1
+        maxLead = p1 - p2
+    elif p2 > p1 and p2 - p1 > maxLead:
+        p_win = 2
+        maxLead = p2 - p1
+
+print(p_win, maxLead)
