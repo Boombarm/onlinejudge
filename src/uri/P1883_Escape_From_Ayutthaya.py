5852152d3b44aaf44f0a82dea779ee77a8813918
Teerapat_ <boombarm@gmail.com>
2018-03-19 17:55:33 +0700

    Add files via upload

---
+++ src/uri/P1883_Escape_From_Ayutthaya.py
@@ -0,0 +1,101 @@
+N = M = 0
+MAP = []
+start = None
+end = None
+fire = None
+
+#top -> bot -> right -> left
+nY = [-1, 1, 0, 0]
+nX = [0, 0, -1, 1]
+
+class Point:
+    x = y = t = 0
+        
+    def __init__(self,y, x, t=0):
+        self.x = x
+        self.y = y
+        self.t = t
+    
+    def equals(self,p):
+        # print("selfX",self.x,"PX", p.x)
+        # print("selfY",self.y,"PY",p.y)
+        # print("Chk ",self.x == p.x and self.y == p.y)
+        return self.x == p.x and self.y == p.y 
+
+    
+def BFS(Q):
+    while(len(Q) > 0):
+        p = Q.pop(0)
+        if p == None: 
+            continue
+    
+        pX = p.x
+        pY = p.y
+        t = p.t
+        MAP[pY][pX] = 1;
+        dead = (abs(fire.x - pX) + abs(fire.y - pY))
+        if dead == t : 
+            continue
+        
+        if p.equals(end):
+            return True
+
+        for i in range(0, 4):
+            pX = p.x + nX[i]
+            pY = p.y + nY[i]
+            if pX > 0 and pX < M and pY > 0 and pY < N :
+                if MAP[pY][pX] == 0 :
+                    nextPoint = Point(pY,pX,t+1)
+                    Q.append(nextPoint)
+    
+    print("End loop")
+    return False
+    
+    
+#Main 
+t = int(input())
+while(t > 0):
+    t = t-1
+    N, M = map(int, input().split())
+    MAP = []
+    start = None
+    end = None
+    fire = None
+    Q = []
+    for y in range(0, N):
+        MAP.append(list(input()))
+        for x in range(len(MAP[y])):
+            if MAP[y][x] == '.':
+                MAP[y][x] = 0
+            elif MAP[y][x] == '#':
+                MAP[y][x] = 1
+            elif MAP[y][x] == 'S':
+                MAP[y][x] = 1
+                start = Point(y,x)
+            elif MAP[y][x] == 'E' :
+                end = Point(y,x);
+            elif MAP[y][x] == 'F' :
+                MAP[y][x] = 1
+                fire = Point(y,x)
+            else:
+                MAP[y][j] = 0
+    
+    # for y in range(0, N):
+    #     print(MAP[y])
+
+    escape = (abs(start.x - start.x) + abs(start.y - start.y))
+    dead = (abs(fire.x - end.x) + abs(fire.y - end.y))
+    if escape >= dead :
+        print("SSSSSS N")
+        continue
+    
+    Q.append(start)
+    answer = BFS(Q)
+    # for y in range(0, N):
+    #     print(MAP[y])
+        
+    if answer :
+        print("Y")
+    else:
+        print("N")
+#End Main 
