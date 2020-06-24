188a8db4253471ff68ae9dba0da824e25ef0e98b
Teerapat <boombarm@gmail.com>
2020-06-24 22:11:48 +0700

    -

---
+++ src/uri/Accepted/BEGINNER/P3091_Rest_1_0.py
@@ -0,0 +1,16 @@
+# author : Teerapat Phokhonwong
+# Online judge: URI Online Judge
+# Categories: AD-HOC
+# Problem: 3091 - Rest 1.0
+# Link: https://www.urionlinejudge.com.br/judge/en/problems/view/3091
+# Timelimit: 1 sec
+# Status: Accepted
+# Submission: 6/24/20, 10:01:21 PM
+# Runtime: 0.032s
+# Solution:
+# Note:
+
+a = int(input())
+b = int(input())
+answer = int(a % b)
+print(answer)
