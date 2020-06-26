4b9ffbe991d61a2ef61d14113c37ded6552b8d9b
Teerapat <boombarm@gmail.com>
2020-06-26 15:37:46 +0700

    -

---
+++ src/uri/P3096.py
@@ -0,0 +1,8 @@
+# length result of factorial 
+for i in range(1,1001):
+    # n = int(input())
+    total = 1
+    for x in range(i,1, -1):
+        total *= x
+    j = len(str(total))
+    print(str(i),j, j-i)
