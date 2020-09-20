3b648680f7a81c786eb3fa0202162d78578eedae
Teerapat <boombarm@gmail.com>
2020-09-20 18:20:40 +0700

    Delete P3096.py

--- src/uri/P3096.py
+++
@@ -1,9 +0,0 @@
-# length result of factorial
-# https://en.wikipedia.org/wiki/Factorial
-for i in range(1,1001):
-    # n = int(input())
-    total = 1
-    for x in range(i,1, -1):
-        total *= x
-    j = len(str(total))
-    print(str(i),j, j-i)
