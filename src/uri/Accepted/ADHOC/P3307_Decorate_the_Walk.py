d5bae7a9c79c083003684035747bee0be1ad88f2
Teerapat Phokhonwong <boombarm@gmail.com>
2021-09-02 00:55:53 +0700

    update 02.09.21

---
+++ src/uri/Accepted/ADHOC/P3307_Decorate_the_Walk.py
@@ -0,0 +1,23 @@
+# PROBLEM: 3307 - Decorate the Walk!
+# Link: https://www.urionlinejudge.com.br/judge/en/problems/view/3307
+# RUNTIME: 0.084s
+# SUBMISSION: 9/1/21, 11:44:29 PM
+# note: find radius from area of circle
+import math
+
+pi = 3.14
+n = int(input())
+for i in range(n):
+    a = int(input())
+    r = math.sqrt(a / (4 * pi))
+    text = cal_rate = None
+    if r < 12:  # red
+        text = 'vermelho'
+        cal_rate = 0.09
+    elif 12 <= r <= 15:  # blue
+        text = 'azul'
+        cal_rate = 0.07
+    else:  # yellow
+        text = 'amarelo'
+        cal_rate = 0.05
+    print('{text} = R$ {cost:.2f}'.format(text=text, cost=a * cal_rate))
