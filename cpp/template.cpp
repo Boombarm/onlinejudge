ec44b39a8a060073b47c6b9bdd7f3752c76e9472
Teerapat Phokhonwong <boombarm@gmail.com>
2022-12-14 01:43:54 +0700

    update

--- cpp/template.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <cmath>
-using namespace std;
-
-int main(){
-    cout << "hello" << endl;
-    return 0;
-}
--- dart/beecrowd/template.dart
+++
@@ -1,6 +0,0 @@
-import 'dart:io';
-
-void main()
-{
-    print("");
-}   
