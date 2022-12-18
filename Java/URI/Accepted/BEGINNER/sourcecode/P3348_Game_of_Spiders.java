0db059277857e4b29b6d0898c52c8aa4def3a97b
Teerapat Phokhonwong <boombarm@gmail.com>
2022-12-18 16:57:28 +0700

    update

---
+++ Java/URI/Accepted/BEGINNER/sourcecode/P3348_Game_of_Spiders.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+import java.io.IOException;
+
+public class P3348_Game_of_Spiders {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        switch (n) {
+            case 1:
+                System.out.println("2");
+                break;
+            case 2:
+                System.out.println("7");
+                break;
+            case 3:
+                System.out.println("5");
+                break;
+            case 4:
+                System.out.println("30");
+                break;
+            case 5:
+                System.out.println("169");
+                break;
+            case 6:
+                System.out.println("441");
+                break;
+            case 7:
+                System.out.println("1872");
+                break;
+            case 8:
+                System.out.println("7632");
+                break;
+            case 9:
+                System.out.println("1740");
+                break;
+            case 10:
+                System.out.println("93313");
+                break;
+            case 11:
+                System.out.println("459901");
+                break;
+            case 12:
+                System.out.println("1358657");
+                break;
+            case 13:
+                System.out.println("2504881");
+                break;
+            case 14:
+                System.out.println("13482720");
+                break;
+            case 15:
+                System.out.println("25779600");
+                break;
+            case 16:
+                System.out.println("68468401");
+                break;
+            case 17:
+                System.out.println("610346880");
+                break;
+            case 18:
+                System.out.println("1271932200");
+                break;
+            case 19:
+                System.out.println("327280800");
+                break;
+        }
+        sc.close();
+    }
+}
