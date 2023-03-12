6db198eac504d9318a45131f0314b60ec6bf508d
Teerapat Phokhonwong <boombarm@gmail.com>
2023-03-12 16:48:41 +0700

    ีupdate

--- Python/src/uri_beecrowd/BEGINNER/P3343_Attack_On_Gasparini.py
+++ Python/src/uri_beecrowd/BEGINNER/P3343_Attack_On_Gasparini.py
@@ -20,20 +20,15 @@ for t in titans:
     wall_level = last_wall[t]
     is_alive = True
     while is_alive:
-        # print("wall_{wall_level} -> {t}{size}".format(wall_level=wall_level, t=t, size=titan_size[t]))
         if k <= wall_remain[wall_level]:
-            # print('{wall_size} - {size} = {remain}'.format(wall_size=wall_remain[wall_level], size=titan_size[t], remain=wall_remain[wall_level] - titan_size[t]))
             wall_remain[wall_level] -= k
-            # print('die')
             is_alive = False
             break
         else:
             wall_level += 1
             last_wall[t] = wall_level
-            # print('jump')
             if wall_level == len(wall_remain):
-                # print('new wall')
                 wall_remain.append(wall_height)
-# print(wall_remain)
+
 total_wall = len(wall_remain)
 print(total_wall)
